53049ea8a92e103416c66d7d1d255088ee576194
troy213 <tritera.erlangga@gmail.com>
2022-02-03 14:40:02 +0700

    Chace Busting and HtmlWebpackPlugin
    -Install HtmlWebpacPlugin
    -Create template.html for dynamic script source
    -Delete index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: 'main.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
